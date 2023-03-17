7a5b6f956a0e3b3b47923b7344a360165f374c8c
Sayantika Dey <Sayantikasuri@gmail.com>
2023-03-17 21:29:14 +0530

    added code

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Dimensions, Button } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import React, {useState, useEffect} from 'react';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [mapRegion, setMapRegion] = useState({});
+  const [lat, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const userLocation = async () => {
+    let {status} = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+    }
+    let location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.Highest});
+    setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.000918,
+      longitudeDelta: 0.000418,
+    });
+    setLatitude(location.coords.latitude);
+    setLongitude(location.coords.longitude);
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map}
+        region={mapRegion}
+        initialRegion={{
+          latitude: 20.5937,
+          longitude: 78.9629,
+          latitudeDelta: 30,
+          longitudeDelta: 30,}}
+          > 
+          {lat && (<Marker coordinate={mapRegion} title='Hi, there'/>)}
+      </MapView>
+      <Button title='Get Location' onPress={userLocation} />
+      
     </View>
   );
 }
@@ -17,4 +49,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map:{
+    width: 400,
+    height: 500
+  },
+  
 });
+
